a755ffa8c5438f33666fcb77ac3c29b16cf5e4be
Freddy Cuellar <facuellarg@unal.edu.co>
2020-08-24 04:03:36 -0500

    Updates

---
+++ precache-manifest.b7135d92792199368e2c2f03be2a2d49.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2338e7afda1224a41431f787ed3678d5",
+    "url": "/front_schedule/index.html"
+  },
+  {
+    "revision": "7a6c3f8446c4e2576a71",
+    "url": "/front_schedule/static/css/main.eb7a2f96.chunk.css"
+  },
+  {
+    "revision": "34132ae39308933502cf",
+    "url": "/front_schedule/static/js/2.2d110dee.chunk.js"
+  },
+  {
+    "revision": "b47b83f293ef0a67523532a1d2a28ee0",
+    "url": "/front_schedule/static/js/2.2d110dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6c3f8446c4e2576a71",
+    "url": "/front_schedule/static/js/main.b615b1f1.chunk.js"
+  },
+  {
+    "revision": "82aedc414d8467f7bb01",
+    "url": "/front_schedule/static/js/runtime-main.602ff275.js"
+  },
+  {
+    "revision": "127a320e3bfd8fb6f82b668ab509383b",
+    "url": "/front_schedule/static/media/ancizar_sans-light-webfont.127a320e.ttf"
+  },
+  {
+    "revision": "62c00607e2da14ba79085cc84244d979",
+    "url": "/front_schedule/static/media/ancizar_sans-light-webfont.62c00607.woff"
+  },
+  {
+    "revision": "76ffcc923a2a061d1ebc69c322a8389f",
+    "url": "/front_schedule/static/media/ancizar_sans-light-webfont.76ffcc92.eot"
+  },
+  {
+    "revision": "f9519dbde443369ad3bfec39d839e844",
+    "url": "/front_schedule/static/media/ancizar_sans-light-webfont.f9519dbd.svg"
+  },
+  {
+    "revision": "e41beee1227cffa2c7c92cae26649492",
+    "url": "/front_schedule/static/media/background.e41beee1.jpg"
+  },
+  {
+    "revision": "466d4d9998a93044b09e22ac535ec5a2",
+    "url": "/front_schedule/static/media/gif1.466d4d99.gif"
+  },
+  {
+    "revision": "e8d37521dc95846366dbc5fb16566131",
+    "url": "/front_schedule/static/media/gif2.e8d37521.gif"
+  },
+  {
+    "revision": "bd9adc7c40dd59a28fd42e092d2c9080",
+    "url": "/front_schedule/static/media/gif3.bd9adc7c.gif"
+  },
+  {
+    "revision": "a293743b27f48726be0b9dd707a711fd",
+    "url": "/front_schedule/static/media/gif4.a293743b.gif"
+  }
+]);
